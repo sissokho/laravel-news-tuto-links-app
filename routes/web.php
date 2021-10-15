4f7849f3137bb28a225989f77c85a7c115a5a9eb
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2021-10-15 19:28:02 +0000

    Show list of links

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Link;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = Link::all();
+
+    return view('welcome', [
+        'links' => $links
+    ]);
 });
 
 Auth::routes();
